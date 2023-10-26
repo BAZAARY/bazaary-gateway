3612ef8d271962478e50a18dd89b38c84a06ae5b
alessandrodiaz <alessdg02@gmail.com>
2023-10-26 09:07:29 -0500

    Deploy Test 1

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloGateway } from '@apollo/gateway';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import { buildFederatedSchema } from '@apollo/federation';
-import http from 'http';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { buildFederatedSchema } from "@apollo/federation";
+import http from "http";
 
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 9000; // Allow configuring the port through an environment variable
+const PORT = process.env.PORT || 4000; // Allow configuring the port through an environment variable
 
 // URLs of your Apollo microservices
-const service1URL = 'https://authentication-bazaary.vercel.app/graphql';
-const service2URL = 'https://catalogmicroservicegraphql.onrender.com/graphql';
+const service1URL = "https://authentication-bazaary.vercel.app/graphql";
+const service2URL = "https://catalogmicroservicegraphql.onrender.com/graphql";
 
 // Configure the Apollo Gateway
+const supergraphSdl = new IntrospectAndCompose({
+	subgraphs: [
+		{ name: "auth", url: service1URL },
+		{ name: "catalog", url: service2URL },
+	],
+});
+
 const gateway = new ApolloGateway({
-  serviceList: [
-    { name: 'service1', url: service1URL },
-    { name: 'service2', url: service2URL },
-  ],
+	supergraphSdl,
+	__exposeQueryPlanExperimental: false,
 });
 
 const httpServer = http.createServer(app);
 
 const startApolloServer = async (app, httpServer) => {
-  const server = new ApolloServer({
-    gateway,
-    subscriptions: false,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+	const server = new ApolloServer({
+		gateway,
+		engine: false,
+		subscriptions: false,
+		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+	});
 
-  await server.start();
+	await server.start();
 
-  server.applyMiddleware({ app });
-}
+	server.applyMiddleware({ app });
+};
 
 startApolloServer(app, httpServer);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+	console.log(`⭐ API GATEWAY Server ready at port ${PORT}`);
 });
