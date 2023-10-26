50f76f7faa392bc6d4eea10230053b4879731f7c
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-10-25 23:05:32 -0500

    a

--- api/app.js
+++ api/app.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const { ApolloGateway } = require('@apollo/gateway');
-const { buildFederatedSchema } = require('@apollo/federation');
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloGateway } from '@apollo/gateway';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { buildFederatedSchema } from '@apollo/federation';
+import http from 'http';
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
+const httpServer = http.createServer(app);
+const PORT = process.env.PORT || 9000; // Allow configuring the port through an environment variable
 
-// URLs de tus microservicios Apollo
-const service1URL = "https://authentication-bazaary.vercel.app/graphql";
-const service2URL = "https://catalogmicroservicegraphql.onrender.com/graphql";
+// URLs of your Apollo microservices
+const service1URL = 'https://authentication-bazaary.vercel.app/graphql';
+const service2URL = 'https://catalogmicroservicegraphql.onrender.com/graphql';
 
-// Configura el servidor Apollo Gateway
+// Configure the Apollo Gateway
 const gateway = new ApolloGateway({
-	serviceList: [
-		{ name: 'service1', url: service1URL },
-		{ name: 'service2', url: service2URL },
-	],
+  serviceList: [
+    { name: 'service1', url: service1URL },
+    { name: 'service2', url: service2URL },
+  ],
 });
 
 async function startApolloServer() {
-	const server = new ApolloServer({
-		gateway,
-		subscriptions: false,
-	});
+  const server = new ApolloServer({
+    gateway,
+    subscriptions: false,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-	await server.start();
+  await server.start();
 
-	server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 }
 
-startApolloServer().then(() => {
+startApolloServer();
 
+httpServer.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
-module.exports = app
