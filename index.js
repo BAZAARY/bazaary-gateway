07e86ccc9d70fc7b4d13b200c5b785b412d92de1
alessandrodiaz <alessdg02@gmail.com>
2023-10-26 09:26:34 -0500

    Vercel.json

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import { buildFederatedSchema } from "@apollo/federation";
 import http from "http";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+const httpServer = http.createServer(app);
+
 const PORT = process.env.PORT || 4000; // Allow configuring the port through an environment variable
 
 // URLs of your Apollo microservices
-const service1URL = "https://authentication-bazaary.vercel.app/graphql";
-const service2URL = "https://catalogmicroservicegraphql.onrender.com/graphql";
+const authenticationURL = "https://authentication-bazaary.vercel.app/graphql";
+const catalogURL = "https://catalogmicroservicegraphql.onrender.com/graphql";
 
 // Configure the Apollo Gateway
 const supergraphSdl = new IntrospectAndCompose({
 	subgraphs: [
-		{ name: "auth", url: service1URL },
-		{ name: "catalog", url: service2URL },
+		{ name: "authentication", url: authenticationURL },
+		{ name: "catalog", url: catalogURL },
 	],
 });
 
@@ -25,8 +30,6 @@ const gateway = new ApolloGateway({
 	supergraphSdl,
 	__exposeQueryPlanExperimental: false,
 });
-
-const httpServer = http.createServer(app);
 
 const startApolloServer = async (app, httpServer) => {
 	const server = new ApolloServer({
