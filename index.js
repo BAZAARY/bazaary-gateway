7277ca5f7747cb13d733f721402f959cf85406a1
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-10-25 22:08:28 -0500

    express

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-const { ApolloServer } = require("apollo-server");
-const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloGateway } = require('@apollo/gateway');
+const { buildFederatedSchema } = require('@apollo/federation');
 
-const supergraphSdl = new IntrospectAndCompose({
-	// This entire subgraph list is optional when running in managed federation
-	// mode, using Apollo Studio as the source of truth.  In production,
-	// using a single source of truth to compose a schema is recommended and
-	// prevents composition failures at runtime using schema validation using
-	// real usage-based metrics.
-	subgraphs: [
-		{
-			name: "authentication",
-			url: "https://authentication-bazaary.vercel.app/graphql",
-		},
-		{
-			name: "catalogmicroservicegraphql",
-			url: "https://catalogmicroservicegraphql.onrender.com/graphql"
-		}
-	],
-});
+const app = express();
+const PORT = 3000;
 
+// URLs de tus microservicios Apollo
+const service1URL = "https://authentication-bazaary.vercel.app/graphql";
+const service2URL = "https://catalogmicroservicegraphql.onrender.com/graphql";
+
+// Configura el servidor Apollo Gateway
 const gateway = new ApolloGateway({
-	supergraphSdl,
-	// Experimental: Enabling this enables the query plan view in Playground.
-	__exposeQueryPlanExperimental: false,
-});
-
-(async () => {
+	serviceList: [
+	  { name: 'service1', url: service1URL },
+	  { name: 'service2', url: service2URL },
+	],
+  });
+  
+  async function startApolloServer() {
 	const server = new ApolloServer({
-		gateway,
-
-		// Apollo Graph Manager (previously known as Apollo Engine)
-		// When enabled and an `ENGINE_API_KEY` is set in the environment,
-		// provides metrics, schema management and trace reporting.
-		engine: false,
-
-		// Subscriptions are unsupported but planned for a future Gateway version.
-		subscriptions: false,
+	  gateway,
+	  subscriptions: false,
 	});
-
-	server.listen().then(({ url }) => {
-		console.log(`⭐ API GATEWAY Server ready at ${url}`);
+  
+	await server.start();
+  
+	server.applyMiddleware({ app });
+  }
+  
+  startApolloServer().then(() => {
+	app.listen(PORT, () => {
+	  console.log(`Server is running on port ${PORT}`);
 	});
-})();
+  });
